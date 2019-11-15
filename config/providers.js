2af3fc31390fd2c25f1697e8f86de9c79a50c184
DavidLazic <incognetii@gmail.com>
2019-11-15 15:04:12 +0100

    Adding Claymore - Manganelo.

--- config/providers.js
+++ config/providers.js
@@ -32,6 +32,23 @@ const PROVIDERS = {
       title: document =>
         document.title.split(' | ')[0].replace(/[\s|:|.|?]/g, '_')
     }
+  },
+  'Manganelo': {
+    url: ({ id, src }) => `${src}/manga-${id}`,
+    chapter: {
+      get: document =>
+        Array
+          .from(document.querySelectorAll('.container-chapter-reader img'))
+          .map(item => item.src)
+      ,
+      getAll: document =>
+        Array
+          .from(document.querySelectorAll('.row-content-chapter a'))
+          .map(item => item.href)
+      ,
+      title: document =>
+        document.title.split(' | ')[0].replace(/[\s|:|.|?]/g, '_')
+    }
   }
 };
 
